23341eea34949776837e14f7de95830341b4d747
ai1ml <157225769+ai1ml@users.noreply.github.com>
2025-10-14 21:26:02 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,3 +1,13 @@
+import boto3, tarfile
+
+s3 = boto3.client("s3")
+s3.download_file("your-bucket-name", "path/to/model.tar.gz", "model.tar.gz")
+
+# extract locally
+with tarfile.open("model.tar.gz") as tar:
+    tar.extractall("model_dir")
+
+
 import tensorflow as tf
 model = tf.saved_model.load("path/to/saved_model")
 sig = model.signatures["serving_default"]
